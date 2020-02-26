f283bfd56a877af79d8c120e7c25de0d9c223247
zaxanq <zaxanq@gmail.com>
2020-02-26 13:52:58 +0100

    feat(display-notes): add max-width for a note and max content length.

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -3,12 +3,16 @@ import './Note.scss';
 import NotesContext from '../../contexts';
 
 const Note = ({ id }) => {
+    const noteContentLengthToDisplay = 255;
     const notes = useContext(NotesContext);
+    const shortenedNoteContent = notes[id].contents.length > noteContentLengthToDisplay
+        ? notes[id].contents.substr(0, noteContentLengthToDisplay) + '...'
+        : notes[id].contents;
 
     return (
         <article className="note">
             <h3 className="note__title">{ notes[id].title }</h3>
-            <p className="note__contents">{ notes[id].contents }</p>
+            <p className="note__contents">{ shortenedNoteContent }</p>
         </article>
     );
 };
